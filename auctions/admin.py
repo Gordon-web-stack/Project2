27973c66868ad3a2fab790193596751a3dd05ae5
Gordon-web-stack <Gmcgaffin567@gmail.com>
2021-02-22 16:21:15 +0200

    Day1
    implemented database for the listings, work is needed to list items on index page.

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class ListAdmin(admin.ModelAdmin):
+    list_display = ("List_name", "List_description")
+
+admin.site.register(Listings, ListAdmin)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listings(models.Model):
+    List_name = models.CharField(max_length = 32)
+    List_description = models.TextField()
+    List_start_price = models.FloatField(default=0)
+    List_image_url = models.CharField(max_length= 64, null=True)
+    
+    def __str__(self):
+        return f"{self.List_name} ({self.List_description})"
